b2d70d4a331c459d70f03f4efb70724c31b061f0
pangjiaqi0923@163.com <pangjiaqi0923@163.com>
2017-09-05 22:36:20 +0800

    Initial commit 0905

---
+++ app/src/main/java/com/greenfinch/sharecars/ContextApplication.java
@@ -0,0 +1,15 @@
+package com.greenfinch.sharecars;
+
+import android.app.Application;
+
+/**
+ * Created by pangjiaqi on 2017/9/5.
+ */
+
+public class ContextApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
